54abda9293a7740826f8f7d2fd71510c505b56df
aaman123 <sutariyaaman1998@gmail.com>
2020-03-10 22:23:07 +0530

    google tag config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+declare var gtag;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'ObsessedWithFitness';
+
+  constructor(private router:Router) {
+    const navEndEvents = router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    )
+
+    navEndEvents.subscribe( (event: NavigationEnd)=> {
+       gtag('config', 'UA-160064411-1' ,{
+         'page_path': event.urlAfterRedirects
+       });
+    })
+  }
 }
